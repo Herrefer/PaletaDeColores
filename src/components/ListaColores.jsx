251735f8f64f4e9bf676933e55ffebc4e4c03f94
Fernando Herrera <herrera770@outlook.es>
2024-01-24 13:47:14 -0300

    se moficiaron la cantidad de colores guardados por defecto

--- src/components/ListaColores.jsx
+++ src/components/ListaColores.jsx
@@ -5,10 +5,6 @@ const ListaColores = () => {
         <div className="d-flex flex-row justify-content-between flex-wrap">
             <ColorGuardado></ColorGuardado>
             <ColorGuardado></ColorGuardado>
-            <ColorGuardado></ColorGuardado>
-            <ColorGuardado></ColorGuardado>
-            <ColorGuardado></ColorGuardado>
-            <ColorGuardado></ColorGuardado>
         </div>
     );
 };
